a731d9fa872a7b49fd21bee80b64b1708fc6256e
robertomardini <robertomardini@gmail.com>
2025-04-20 12:04:11 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 (async () => {
   // 1) Instancia Supabase
   const SUPABASE_URL      = 'https://evwdjscgstgbzvbunhpd.supabase.co';
-  const SUPABASE_ANON_KEY = 'YOUR_ANON_KEY';
+  const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2d2Rqc2Nnc3RnYnp2YnVuaHBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ3NjEyOTMsImV4cCI6MjA2MDMzNzI5M30.sG08lWD-kSvWc_lwq43N_BPoswZq0yRpz_-uC7HDfnI';
   window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
   // 2) Obtener elementos
