fc08fecdd488c5eaaede94c40bd2380fa109d877
robertomardini <robertomardini@gmail.com>
2025-04-20 22:09:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 const body = document.querySelector('body'),
       sidebar = body.querySelector('nav'),
-      toggle = body.querySelector(".toggle"),
-      searchBtn = body.querySelector(".search-box"),
+      toggle = body.querySelector(".toggle"), 
       modeSwitch = body.querySelector(".toggle-switch"),
       modeText = body.querySelector(".mode-text");
+      searchBtn = body.querySelector(".search-box"),
 
 
 toggle.addEventListener("click" , () =>{
     sidebar.classList.toggle("close");
 })
 
-searchBtn.addEventListener("click" , () =>{
-    sidebar.classList.remove("close");
-})
+if (searchBtn) {
+  searchBtn.addEventListener("click", () =>{
+      sidebar.classList.remove("close");
+  });
+}
 
-modeSwitch.addEventListener("click" , () =>{
-    body.classList.toggle("dark");
-    
-    if(body.classList.contains("dark")){
-        modeText.innerText = "Light mode";
-    }else{
-        modeText.innerText = "Dark mode";
-        
-    }
-});
+if (modeSwitch) {
+   modeSwitch.addEventListener("click", () =>{
+     body.classList.toggle("dark");
+
+     if (body.classList.contains("dark")) {
+       modeText.innerText = "Light mode";
+     } else {
+       modeText.innerText = "Dark mode";
+     }
+   });
+ }
